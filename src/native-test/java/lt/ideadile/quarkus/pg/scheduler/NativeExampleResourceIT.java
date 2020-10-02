03a59af1da1be0618484bd22ab1bb7a4d210f4a9
quarkusio <no-reply@quarkus.io>
2020-10-02 07:59:06 +0000

    Initial commit

---
+++ src/native-test/java/lt/ideadile/quarkus/pg/scheduler/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package lt.ideadile.quarkus.pg.scheduler;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
